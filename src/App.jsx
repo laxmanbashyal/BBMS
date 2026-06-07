ae6ac4eaedad869bd0bf244af607e1f269f20811
Laxman Bashyal <laxmanbashyal913@gmail.com>
2026-06-07 08:37:31 +0545

    style navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
+
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import BloodAvailability from "./pages/BloodAvailability";
 import DonationProgram from "./pages/DonationProgram";
 
+
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,42 @@
 import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/blood-avalebility">BA</Link>
-        </li>
-        <li>
-          {" "}
-          <Link to="/login">LOGIN</Link>
-        </li>
-        <li>
-          <Link to="/register">REGISTER</Link>
-        </li>
-        <li>
-          <Link to="donation-program">Donation</Link>{" "}
-        </li>
-      </ul>
+    <nav className="bg-white shadow-md sticky top-0 z-0">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+        <h1 className="text-2xl font-bold text-red-600">🩸 BBMS</h1>
+        <ul className="flex gap-8 items-center font-medium text-gray-700">
+          <li>
+            <Link to="/" className="hover:text-red-600 transition mar">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/blood-avalebility"
+              className="hover:text-red-600 transition"
+            >
+              Blood Availebility
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="donation-program"
+              className="hover:text-red-600 transition"
+            >
+              Donation
+            </Link>{" "}
+          </li>
+        </ul>{" "}
+        <div> <button> <Link to="/login" className="hover:text-red-600 border p-2 rounded-md transition">
+          LOGIN
+        </Link></button>
+        <button className=""> <Link to="/register" className="text-white rounded-md p-2  bg-red-600 m-5">
+          REGISTER
+        </Link></button></div>
+
+       
+       
+      </div>
     </nav>
   );
 };
